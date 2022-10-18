004736578c75f398d340d57148b982f5a5273b7c
Ryan Lowe <somebody480@gmail.com>
2022-10-18 17:29:12 -0400

    chunkArray test complete

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -4,9 +4,76 @@
  E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+// first pass
 function chunkArray(array, size) {
-    // Code goes here
+  let giga = [];
+  let smol = [];
+  let sizeIterator = 0;
+  for (let i = 0; i < array.length; i++) {
+    smol.push(array[i]);
+    if (i === array.length - 1) {
+      giga.push(smol);
+      break;
+    }
+    if (sizeIterator === size - 1) {
+      giga.push(smol);
+      smol = [];
+      sizeIterator = 0;
+    } else {
+      sizeIterator++;
+    }
+  }
+  return giga;
+}
+// looping
+function chunkArray(array, size) {
+  let result = [];
+  for (value of array) {
+    let lastArray = result[result.length - 1];
+    if (!lastArray || lastArray.length === size) {
+      result.push([value]);
+    } else {
+      lastArray.push(value);
+    }
+  }
+  return result;
 }
 
+// splice
+function chunkArray(array, size) {
+  let result = [];
+  let arrayCopy = [...array];
+  while (arrayCopy.length > 0) {
+    result.push(arrayCopy.splice(0, size));
+  }
+  return result;
+}
 
-module.exports = chunkArray
+//slice
+function chunkArray(array, size) {
+  let result = [];
+  for (let i = 0; i < array.length; i += size) {
+    let chunk = array.slice(i, i + size);
+    result.push(chunk);
+  }
+  return result;
+}
+
+// recursion
+function chunkArray(array, size) {
+  if (array.length <= size) {
+    return [array];
+  }
+  return [array.slice(0, size), ...chunkArray(array.slice(size), size)];
+}
+// test is chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5)
+
+/* --- TEST RESULTS
+first pass: 3.75% slower
+looping: 81.96% slower
+splice: 75.37% slower
+slice: fastest
+recursion: 54.76% slower
+*/
+
+module.exports = chunkArray;
