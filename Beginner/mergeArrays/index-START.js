fa645d424356609f8eff2d8e8acbd34e03041256
Ryan Lowe <somebody480@gmail.com>
2022-10-18 17:42:49 -0400

    mergeArrays init code complete

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -4,18 +4,56 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+// first pass
 function mergeArrays(...arrays) {
+  const set = new Set();
+  arrays.forEach((array) => {
+    for (let i = 0; i < array.length; i++) {
+      set.add(array[i]);
+    }
+  });
+  return new Array(set);
+}
+// using Sets
+function mergeArrays(...arrays) {
+  let jointArray = [];
 
-    let jointArray = []
-    
-    arrays.forEach(array => {
-        jointArray = [...jointArray, ...array]
-    });
+  arrays.forEach((array) => {
+    jointArray = [...jointArray, ...array];
+  });
 
-    return [...new Set([...jointArray])]
-
-    
+  return [...new Set([...jointArray])];
 }
 
+// using .filter()
+function mergeArrays(...arrays) {
+  let jointArray = [];
 
-module.exports = mergeArrays
+  arrays.forEach((array) => {
+    jointArray = [...jointArray, ...array];
+  });
+
+  const uniqueArray = jointArray.filter(
+    (item, index) => jointArray.indexOf(item) === index
+  );
+  return uniqueArray;
+}
+
+// using .reduce()
+function mergeArrays(...arrays) {
+  let jointArray = [];
+
+  arrays.forEach((array) => {
+    jointArray = [...jointArray, ...array];
+  });
+  const uniqueArray = jointArray.reduce((newArray, item) => {
+    if (newArray.includes(item)) {
+      return newArray;
+    } else {
+      return [...newArray, item];
+    }
+  }, []);
+  return uniqueArray;
+}
+
+module.exports = mergeArrays;
