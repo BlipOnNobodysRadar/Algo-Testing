67886993f8c53480468d48dd5b415c6767e92eb0
Ryan Lowe <somebody480@gmail.com>
2022-10-18 16:30:18 -0400

    longestWord init code complete

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -3,11 +3,44 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-
-
+// first pass
 function longestWord(text) {
-    // Code goes here
+  return text.split(" ").reduce((longest, word) => {
+    return word.length > longest ? word : longest;
+  }, "");
+}
+// for loop
+function longestWord(text) {
+  let wordArray = text.split(" ");
+  let maxLength = 0;
+  let result = "";
+  for (let i = 0; i < wordArray.length; i++) {
+    if (wordArray[i].length > maxLength) {
+      maxLength = wordArray[i].length;
+      result = wordArray[i];
+    }
+  }
+  return result;
 }
 
+// reduce
+function longestWord(text) {
+  let result = text.split(" ").reduce((maxLengthWord, currentWord) => {
+    if (currentWord.length > maxLengthWord.length) {
+      return currentWord;
+    } else {
+      return maxLengthWord;
+    }
+  }, "");
+  return result;
+}
 
-module.exports = longestWord
+// .sort()
+function longestWord(text) {
+  var sortedArray = text
+    .split(" ")
+    .sort((wordA, wordB) => wordB.length - wordA.length);
+  return sortedArray[0];
+}
+
+module.exports = longestWord;
