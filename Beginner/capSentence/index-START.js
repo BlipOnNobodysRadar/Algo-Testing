be3535b95aad3fe871ef1a931d2574aa21ddcb3e
Ryan Lowe <somebody480@gmail.com>
2022-10-18 14:48:58 -0400

    capSentence init code complete

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -4,13 +4,39 @@ return the equivalent of the sentence when capitalised. E.g
   capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
 */
 
-
-
-
+// mine
 function capSentence(text) {
-   // Code goes here
+  return text
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
 }
 
+//forEach
+function capSentence(text) {
+  let wordsArray = text.toLowerCase().split("");
+  let capsArray = [];
+  wordsArray.forEach((word) => {
+    capsArray.push(word[0].toUpperCase() + word.slice(1));
+  });
+  return capsArray.join(" ");
+}
 
+// .map() and .slice()
+function capSentence(text) {
+  let wordsArray = text.toLowerCase().split(" ");
+  let capsArray = wordsArray.map((word) => {
+    return word[0].toUpperCase() + word.slice(1);
+  });
+  return capsArray.join(" ");
+}
+// .map() and .replace()
+function capSentence(text) {
+  let wordsArray = text.toLowerCase().split(" ");
+  let capsArray = wordsArray.map((word) => {
+    return word.replace(word[0], word[0].toUpperCase());
+  });
+  return capsArray.join(" ");
+}
 
-module.exports = capSentence
+module.exports = capSentence;
