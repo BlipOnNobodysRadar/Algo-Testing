63fe89df4534c7cc876b446203f69a55c3d5a91c
Ryan Lowe <somebody480@gmail.com>
2022-10-18 16:43:08 -0400

    searchReplace init code complete

---
+++ Beginner/searchReplace/index-START.js
@@ -0,0 +1,27 @@
+// No code was provided in the searchPlace directory.
+
+/*
+Receiving a sentence, a word in the sentence and a replacement for that word as arguments, perform a search and replace on the sentence using the arguments provided and return the new sentence. E.g 
+searchReplace("He is Sleeping on the couch", "Sleeping", "sitting") 
+// should return "He is Sitting on the couch"
+Note: Make sure to match the casing(capitalization) of the string to be replaced as shown above.
+*/
+
+//using replace()
+function searchReplace(str, word, newWord) {
+  // matches capitalization
+  if (word[0] === word[0].toUpperCase()) {
+    newWord = newWord[0].toUpperCase() + newWord.slice(1);
+  }
+  return str.replace(word, newWord);
+}
+
+//using regex
+function searchReplace(str, word, newWord) {
+  let regex = new RegExp(word, "gi");
+  // matches capitalization
+  if (/[A-Z]/.test(word[0])) {
+    newWord = newWord.charAt(0).toUpperCase() + newWord.slice(1);
+  }
+  return str.replace(regex, newWord);
+}
