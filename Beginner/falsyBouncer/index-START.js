b019f4b68bb926e270e468f1a3370253c84b6b16
Ryan Lowe <somebody480@gmail.com>
2022-10-18 18:33:42 -0400

    falsyBouncer init code complete

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -5,10 +5,17 @@ and return an array of only truthy values.
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
-
+// first pass (for loop)
 function falsyBouncer(array) {
-    // Code goes here
+  const result = [];
+  for (let val of array) {
+    if (val) result.push(val);
+  }
+  return result;
+}
+// filter
+function falsyBouncer(array) {
+  return array.filter((value) => value);
 }
 
-
-module.exports = falsyBouncer
+module.exports = falsyBouncer;
