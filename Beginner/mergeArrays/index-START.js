03f649d7c4beefa5fadc4b873b7d92ddb397bd15
Ryan Lowe <somebody480@gmail.com>
2022-10-18 17:52:20 -0400

    mergeArrays testing complete

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -56,4 +56,12 @@ function mergeArrays(...arrays) {
   return uniqueArray;
 }
 
+// test on mergeArrays([1,2,3,3,3], [1,4,5,2], [1,4,5,6,7,1,3,453,5412,14,24,16,75],[23,5,31,46,63,12,123,1,2,4,79,6])
+/* --- TEST RESULTS
+first pass: fastest. Go me.
+Sets: 36.64% slower (I believe the way they merge arrays is unnecessarily inefficient)
+filter: 21.95% slower
+.reduce(): 61.81% slower
+*/
+
 module.exports = mergeArrays;
