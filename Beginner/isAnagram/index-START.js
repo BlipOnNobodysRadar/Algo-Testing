f12af396c044aea7a932c0883af0244be9ce8a20
Ryan Lowe <somebody480@gmail.com>
2022-10-18 16:59:42 -0400

    isAnagram init code complete

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -5,11 +5,47 @@ don't. E.g
     isAnagram('silent', 'listen') // should return true
 */
 
-
-
+// direct comparison
 function isAnagram(stringA, stringB) {
-    // Code goes here
+  const sanitizeString = function (str) {
+    return str
+      .toLowerCase()
+      .replace(/[^a-z\d]/g, "")
+      .split("")
+      .sort()
+      .join("");
+  };
+  return sanitizeString(stringA) === sanitizeString(stringB);
 }
 
+// direct comparison
+function isAnagram(stringA, stringB) {
+  function createCharMap(text) {
+    let charMap = {};
 
-module.exports = isAnagram
+    for (let char of text) {
+      if (charMap.hasOwnProperty(char)) {
+        charMap[char]++;
+      } else {
+        charMap[char] = 1;
+      }
+    }
+
+    return charMap;
+  }
+
+  if (stringA.length === stringB.length) {
+    let stringAMap = createCharMap(stringA);
+    let stringBMap = createCharMap(stringB);
+    for (let char in stringAMap) {
+      if (stringAMap[char] !== stringBMap[char]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+module.exports = isAnagram;
