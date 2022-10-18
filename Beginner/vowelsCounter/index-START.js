c80b3f59151466341fb60f53cc8e391ec2871eca
Ryan Lowe <somebody480@gmail.com>
2022-10-18 14:13:06 -0400

    vowelsCounter test resulsts

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,7 +3,7 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
-function vowelsCounter(text) {
+function vowelsCounter1(text) {
   let count = 0;
   let vowels = "aeiou".split("");
   for (letter of text) {
@@ -11,9 +11,14 @@ function vowelsCounter(text) {
   }
   return count;
 }
-function vowelsCounter(text) {
+function vowelsCounter2(text) {
   let matchingInstances = text.match(/[aeiou]/gi);
   return matchingInstances ? matchingInstances.length : 0;
 }
 
+/* ---- TEST RESULTS
+vowelsCounter1: 362110 ops/s (83.9%) slower
+vowelsCounter2: 2249746 ops/s (fastest) 
+
+*/
 module.exports = vowelsCounter;
