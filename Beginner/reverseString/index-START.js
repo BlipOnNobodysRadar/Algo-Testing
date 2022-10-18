94174b3d633b0a12378971c1e050934c150f118b
Ryan Lowe <somebody480@gmail.com>
2022-10-18 14:04:40 -0400

    vowelsCounter start complete

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,12 +3,35 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-
-
 function reverseString(text) {
-    // Code goes here
+  return text.split("").reverse().join("");
+}
+function reverseString(text) {
+  return [...text].reverse().join("");
+}
+function reverseString() {
+  let str = "";
+  for (let i = text.length - 1; i > 0; i--) {
+    str += text[i];
+  }
+  return str;
+}
+function reverseString() {
+  let result = "";
+  for (let char of text) {
+    // putting char before result is what reverses the string
+    result = char + result;
+  }
+}
+function reverseString(text) {
+  if (text === "") {
+    return "";
+  } else {
+    return reverseString(text.substr(1) + text[0]);
+  }
+}
+function reverseString(text) {
+  return text.split("").reduce((acc, char) => char + acc, "");
 }
 
-
-
-module.exports = reverseString
+module.exports = reverseString;
--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,11 +3,17 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
-
 function vowelsCounter(text) {
-    // Code goes here
+  let count = 0;
+  let vowels = "aeiou".split("");
+  for (letter of text) {
+    if (vowels.includes(letter.toLowerCase())) count++;
+  }
+  return count;
 }
-
-
+function vowelsCounter(text) {
+  let matchingInstances = text.match(/[aeiou]/gi);
+  return matchingInstances ? matchingInstances.length : 0;
+}
 
 module.exports = vowelsCounter;
